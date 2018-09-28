ba17307e22f074b892cdb224d636a32deef257bb
L. Lester Johnson <shuai.jan28@gmail.com>
2018-09-28 14:56:25 -0400

    Setup CanvassJS for poll result graph on frontend

--- public/main.js
+++ public/main.js
@@ -18,3 +18,52 @@ form.addEventListener("submit", e => {
 
   e.preventDefault();
 });
+
+//CanvasJs
+
+let dataPoints = [
+  { label: "Windows", y: 0 },
+  { label: "MacOS", y: 0 },
+  { label: "Linux", y: 0 },
+  { label: "Others", y: 0 }
+];
+
+const chartContainer = document.querySelector("#chartContainer");
+
+if (chartContainer) {
+  const chart = new CanvasJS.Chart("chartContainer", {
+    animationEnabled: true,
+    theme: "theme1",
+    title: {
+      text: "OS Results"
+    },
+    data: [
+      {
+        type: "column",
+        dataPoints: dataPoints
+      }
+    ]
+  });
+  chart.render();
+
+  // Enable pusher logging - don't include this in production
+  Pusher.logToConsole = true;
+
+  let pusher = new Pusher("da78699db069dc034f2b", {
+    cluster: "us2",
+    forceTLS: true
+  });
+
+  let channel = pusher.subscribe("os-poll");
+  channel.bind("os-vote", function(data) {
+    dataPoints = dataPoints.map(x => {
+      if (x.label == data.os) {
+        x.y += data.points;
+        return x;
+      } else {
+        return x;
+      }
+    });
+    chart.render();
+  });
+}
