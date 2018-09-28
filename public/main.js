c1b12c6f055a86e46edfbc052b5237753acae1d7
L. Lester Johnson <shuai.jan28@gmail.com>
2018-09-28 12:17:12 -0400

    Setup Form Submit event to capture registered votes

---
+++ public/main.js
@@ -0,0 +1,20 @@
+//Form submit Event
+const form = document.getElementById("vote-form");
+
+form.addEventListener("submit", e => {
+  const choice = document.querySelector("input[name=os]:checked").value;
+  const data = { os: choice };
+
+  fetch("http://localhost:3000/poll", {
+    method: "post",
+    body: JSON.stringify(data),
+    headers: new Headers({
+      "Content-Type": "application/json"
+    })
+  })
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+
+  e.preventDefault();
+});
